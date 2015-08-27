7a44a8b01035aa88699026b0511d20972e005394
Ryan Hoover <ryan.hoover@wpengine.com>
2015-08-27 11:11:00 -0500

    first commit

---
+++ lib/core.php
@@ -0,0 +1,23 @@
+<?php
+
+class WPE_Plugin {
+
+	public static function get_instance() {
+
+        static $instance = null;
+
+        if ( null === $instance )
+			$instance = new static();
+
+        return $instance;
+    }
+
+	private function __clone(){
+    }
+
+    private function __wakeup(){
+    }
+
+	protected function __construct() {
+	}
+}
